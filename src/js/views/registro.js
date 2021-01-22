f91808aa3ae66180a07c274ff2d072e5888ae6d7
MartinGO11 <ogutierrez.m94@gmail.com>
2021-01-22 16:12:12 +0000

    registroview

--- src/js/views/registro.js
+++ src/js/views/registro.js
@@ -1,8 +1,60 @@
-import React from "react";
-import "../../styles/home.scss";
+import React, { useState } from "react";
+import Registroimg1 from "../../img/auto-registro.jpg";
+import Registroimg2 from "../../img/bg-registro-2.jpg";
+import Registroimg3 from "../../img/bg-registro.jpg";
+import "../../styles/index.scss";
 
 export const Registro = () => (
-	<div className="text-center mt-5">
-		<h1>Test Registro</h1>
+	<div className="contenedor-principal">
+		<div className="container">
+			<div className="row modal-registro">
+				<div className="col-6 columna-img">
+					<div className="col mr-auto columna-form">
+						<h3 className="titulos-interiores">Registrarse</h3>
+						<div className="parrafo-intro">
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+						</div>
+						<form>
+							<div className="form-group">
+								<input
+									type="text"
+									className="form-control"
+									id="exampleInputName1"
+									aria-describedby="namelHelp"
+									placeholder="Nombre..."
+								/>
+							</div>
+							<div className="form-group">
+								<input
+									type="text"
+									className="form-control"
+									id="exampleInputApellido"
+									placeholder="Apellido..."
+								/>
+							</div>
+							<div className="form-group">
+								<input
+									type="email"
+									className="form-control"
+									id="exampleInputEmail1"
+									placeholder="Email..."
+								/>
+							</div>
+							<div className="form-group">
+								<input
+									type="tel"
+									className="form-control"
+									id="exampleInputTel1"
+									placeholder="Teléfono..."
+								/>
+							</div>
+							<button type="submit" className="btn btn-gold">
+								Registrarme
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 );
