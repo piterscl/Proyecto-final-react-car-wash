05e0b192697d3a1b4903d828db751a49a44246d5
piterscl <pedro.antolin@gmail.com>
2021-02-14 02:03:11 +0000

    modificar columas img

--- src/js/views/registro.js
+++ src/js/views/registro.js
@@ -5,57 +5,55 @@ import Registroimg3 from "../../img/bg-registro.jpg";
 import "../../styles/index.scss";
 
 export const Registro = () => (
-  <div className="contenedor-principal">
-    <div className="container">
-      <div className="row modal-registro">
-        <div className="col-6 columna-img">
-          <div className="col mr-auto columna-form">
-            <h3 className="titulos-interiores">Registrarse</h3>
-            <div className="parrafo-intro">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry.
-            </div>
-            <form>
-              <div className="form-group">
-                <input
-                  type="text"
-                  className="form-control"
-                  id="exampleInputName1"
-                  aria-describedby="namelHelp"
-                  placeholder="Nombre..."
-                />
-              </div>
-              <div className="form-group">
-                <input
-                  type="text"
-                  className="form-control"
-                  id="exampleInputApellido"
-                  placeholder="Apellido..."
-                />
-              </div>
-              <div className="form-group">
-                <input
-                  type="email"
-                  className="form-control"
-                  id="exampleInputEmail1"
-                  placeholder="Email..."
-                />
-              </div>
-              <div className="form-group">
-                <input
-                  type="tel"
-                  className="form-control"
-                  id="exampleInputTel1"
-                  placeholder="Teléfono..."
-                />
-              </div>
-              <button type="submit" className="btn btn-gold">
-                Registrarme
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+	<div className="contenedor-principal">
+		<div className="container">
+			<div className="row modal-registro">
+				<div className="col-6 columna-img" />
+				<div className="col mr-auto columna-form">
+					<h3 className="titulos-interiores">Registrarse</h3>
+					<div className="parrafo-intro">
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+					</div>
+					<form>
+						<div className="form-group">
+							<input
+								type="text"
+								className="form-control"
+								id="exampleInputName1"
+								aria-describedby="namelHelp"
+								placeholder="Nombre..."
+							/>
+						</div>
+						<div className="form-group">
+							<input
+								type="text"
+								className="form-control"
+								id="exampleInputApellido"
+								placeholder="Apellido..."
+							/>
+						</div>
+						<div className="form-group">
+							<input
+								type="email"
+								className="form-control"
+								id="exampleInputEmail1"
+								placeholder="Email..."
+							/>
+						</div>
+						<div className="form-group">
+							<input
+								type="tel"
+								className="form-control"
+								id="exampleInputTel1"
+								placeholder="Teléfono..."
+							/>
+						</div>
+						<button type="submit" className="btn btn-gold">
+							Registrarme
+						</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
 );
