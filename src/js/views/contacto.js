ca766f947c50a3269df3d6a8eff88d67af5d41fd
piterscl <pedro.antolin@gmail.com>
2021-02-22 14:24:57 +0000

    se modifica layout para crear 2 columnas

--- src/js/views/contacto.js
+++ src/js/views/contacto.js
@@ -20,71 +20,71 @@ export const Contacto = () => (
 					<div className="contact-number">
 						<i className="fab fa-whatsapp" /> +569 5555 55 55
 					</div>
-					<div className="col mr-auto">
-						<div id="accordion">
-							<div className="card faq-card">
-								<div className="card-header header-soporte" id="headingOne">
-									<h5 className="mb-0">
-										<button
-											className="btn btn-link faq"
-											data-toggle="collapse"
-											data-target="#collapseOne"
-											aria-expanded="true"
-											aria-controls="collapseOne">
-											F.A.Q #1 <i className="fas fa-angle-down rotate-icon" />
-										</button>
-									</h5>
-								</div>
+				</div>
+				<div className="col mr-auto">
+					<div id="accordion">
+						<div className="card faq-card">
+							<div className="card-header header-soporte" id="headingOne">
+								<h5 className="mb-0">
+									<button
+										className="btn btn-link faq"
+										data-toggle="collapse"
+										data-target="#collapseOne"
+										aria-expanded="true"
+										aria-controls="collapseOne">
+										F.A.Q #1 <i className="fas fa-angle-down rotate-icon" />
+									</button>
+								</h5>
+							</div>
 
-								<div
-									id="collapseOne"
-									className="collapse show"
-									aria-labelledby="headingOne"
-									data-parent="#accordion">
-									<div className="card-body">Anim pariatur cliche reprehenderit.</div>
-								</div>
+							<div
+								id="collapseOne"
+								className="collapse show"
+								aria-labelledby="headingOne"
+								data-parent="#accordion">
+								<div className="card-body">Anim pariatur cliche reprehenderit.</div>
 							</div>
-							<div className="card faq-card">
-								<div className="card-header header-soporte" id="headingTwo">
-									<h5 className="mb-0">
-										<button
-											className="btn btn-link collapsed faq"
-											data-toggle="collapse"
-											data-target="#collapseTwo"
-											aria-expanded="false"
-											aria-controls="collapseTwo">
-											F.A.Q #2 <i className="fas fa-angle-down rotate-icon" />
-										</button>
-									</h5>
-								</div>
-								<div
-									id="collapseTwo"
-									className="collapse"
-									aria-labelledby="headingTwo"
-									data-parent="#accordion">
-									<div className="card-body">Anim pariatur cliche reprehenderit.</div>
-								</div>
+						</div>
+						<div className="card faq-card">
+							<div className="card-header header-soporte" id="headingTwo">
+								<h5 className="mb-0">
+									<button
+										className="btn btn-link collapsed faq"
+										data-toggle="collapse"
+										data-target="#collapseTwo"
+										aria-expanded="false"
+										aria-controls="collapseTwo">
+										F.A.Q #2 <i className="fas fa-angle-down rotate-icon" />
+									</button>
+								</h5>
 							</div>
-							<div className="card faq-card">
-								<div className="card-header header-soporte" id="headingThree">
-									<h5 className="mb-0">
-										<button
-											className="btn btn-link collapsed faq"
-											data-toggle="collapse"
-											data-target="#collapseThree"
-											aria-expanded="false"
-											aria-controls="collapseThree">
-											F.A.Q #3 <i className="fas fa-angle-down rotate-icon" />
-										</button>
-									</h5>
-								</div>
-								<div
-									id="collapseThree"
-									className="collapse"
-									aria-labelledby="headingThree"
-									data-parent="#accordion">
-									<div className="card-body">Anim pariatur cliche reprehenderit.</div>
-								</div>
+							<div
+								id="collapseTwo"
+								className="collapse"
+								aria-labelledby="headingTwo"
+								data-parent="#accordion">
+								<div className="card-body">Anim pariatur cliche reprehenderit.</div>
+							</div>
+						</div>
+						<div className="card faq-card">
+							<div className="card-header header-soporte" id="headingThree">
+								<h5 className="mb-0">
+									<button
+										className="btn btn-link collapsed faq"
+										data-toggle="collapse"
+										data-target="#collapseThree"
+										aria-expanded="false"
+										aria-controls="collapseThree">
+										F.A.Q #3 <i className="fas fa-angle-down rotate-icon" />
+									</button>
+								</h5>
+							</div>
+							<div
+								id="collapseThree"
+								className="collapse"
+								aria-labelledby="headingThree"
+								data-parent="#accordion">
+								<div className="card-body">Anim pariatur cliche reprehenderit.</div>
 							</div>
 						</div>
 					</div>
