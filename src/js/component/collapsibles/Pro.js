acc06d61d646c9cbe8ef9e6f3faeb30e6c7545d3
MartinGO11 <ogutierrez.m94@gmail.com>
2021-02-24 18:52:40 +0000

    collapsibleservices ready

---
+++ src/js/component/collapsibles/Pro.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Collapsible from "react-collapsible";
+
+const Pro = () => {
+	return (
+		<Collapsible trigger="Servicio Pro">
+			<p>- Lavado de carrocería.</p>
+			<p>- Limpieza de vidrios exterior.</p>
+			<p>- Limpieza de plásticos y molduras exteriores.</p>
+			<p>- Renovador de gomas en neumáticos.</p>
+			<p>- Aspirado interior</p>
+			<p>- Aspirado maleta.</p>
+			<p>- Limpieza de tablero y plásticos con producto para limpieza de interiores.</p>
+			<p>- Limpieza interior de vidrios.</p>
+		</Collapsible>
+	);
+};
+
+export default Pro;
