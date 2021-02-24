6ac8c9bfc067f0b674d440df9c56fab92aaa9c5d
MartinGO11 <ogutierrez.m94@gmail.com>
2021-02-24 18:52:31 +0000

    collapsibleservice ready

---
+++ src/js/component/collapsibles/Premium.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Collapsible from "react-collapsible";
+
+const Premium = () => {
+	return (
+		<Collapsible trigger="Servicio Premium">
+			<p>- Lavado de carrocería.</p>
+			<p>- Limpieza de vidrios y espejos exterior e interior.</p>
+			<p>- Limpieza de plásticos y molduras exteriores.</p>
+			<p>- Renovador de gomas en neumáticos.</p>
+			<p>- Aspirado interior y maleta.</p>
+			<p>- Limpieza profunda y detallada de tablero y plásticos puertas, con paño y producto para interiores.</p>
+			<p>- Encerado de carrocería con cera líquida.</p>
+		</Collapsible>
+	);
+};
+
+export default Premium;
